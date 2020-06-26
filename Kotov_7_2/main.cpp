dee00f30b443d468ba5ea911fc590954b65dce12
sampletext32 <sampletext32@bk.ru>
2020-06-26 23:10:46 +0300

    Add project

---
+++ Kotov_7_2/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void randomize(int* array, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = rand() % 100;
+	}
+}
+
+void copy_mass(int* src, int* dest, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		dest[i] = src[i];
+	}
+}
+
+void show_mass(int* array, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << " ";
+	}
+	cout << "\n";
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Выполнил Котов А.А. УМЛ-112\n";
+	cout << "Программа для создания и копирования массива\n";
+	int size = 10;
+	int* arr = new int[size];
+	int* arr2 = new int[size];
+	randomize(arr, size);
+	copy_mass(arr, arr2, size);
+	cout << "Случайно сгенерированный массив: ";
+	show_mass(arr, size);
+	cout << "Скопированный массив: ";
+	show_mass(arr2, size);
+	system("pause");
+}
